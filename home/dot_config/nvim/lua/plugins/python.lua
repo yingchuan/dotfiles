e0bc8fdec2d573df879a0dbb929a528623b82c6e
Richard Chen <yingchuan.chen.2007@gmail.com>
2024-08-18 07:51:21 +0800

    update nvim config plugin python - format on save

--- home/dot_config/nvim/lua/plugins/python.lua
+++ home/dot_config/nvim/lua/plugins/python.lua
@@ -26,11 +26,11 @@ return {
       formatters_by_ft = {
         ["python"] = { "black", "isort" },
       },
-      format_on_save = {
-        -- These options will be passed to conform.format()
-        timeout_ms = 500,
-        lsp_format = "fallback",
-      },
+    },
+    format_on_save = {
+      -- These options will be passed to conform.format()
+      timeout_ms = 500,
+      lsp_format = "fallback",
     },
   },
 }
