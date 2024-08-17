aa57dcee0bbb4960bb2b13cfacba1e1bfcc6b2b9
Richard Chen <yingchuan.chen.2007@gmail.com>
2024-08-18 07:11:24 +0800

    update nvim config plugin python - isort

--- home/dot_config/nvim/lua/plugins/python.lua
+++ home/dot_config/nvim/lua/plugins/python.lua
@@ -1,10 +1,31 @@
 return {
-  "williamboman/mason.nvim",
-  opts = {
-    ensure_installed = {
-      "pyright",
-      "ruff",
-      "black",
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "pyright",
+        "ruff",
+        "black",
+        "isort",
+      },
+    },
+  },
+  {
+    "nvimtools/none-ls.nvim",
+    optional = true,
+    opts = function(_, opts)
+      local nls = require("null-ls")
+      opts.sources = opts.sources or {}
+      table.insert(opts.sources, nls.builtins.formatting.black)
+    end,
+  },
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        ["python"] = { "black", "isort" },
+      },
     },
   },
 }
