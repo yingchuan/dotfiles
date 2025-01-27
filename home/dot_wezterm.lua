07faf1f72ffc31f51a001729532d2e0d74389ac4
Richard Chen <richard.chen@ui.com>
2025-01-28 01:31:16 +0800

    add wezterm config

---
+++ home/dot_wezterm.lua
@@ -0,0 +1,8 @@
+local wezterm = require("wezterm")
+local config = {}
+
+config.color_scheme = "Belge (terminal.sexy)"
+config.font = wezterm.font("JetBrains Mono")
+config.font_size = 14
+
+return config
