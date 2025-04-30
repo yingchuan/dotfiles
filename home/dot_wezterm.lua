33aa9c6bc902195ba0543d8e3f177035a177e332
Richard Chen <richard.chen@ui.com>
2025-05-01 06:30:38 +0800

    python uv python for neovim issues

--- home/dot_wezterm.lua
+++ home/dot_wezterm.lua
@@ -5,4 +5,20 @@ config.color_scheme = "Belge (terminal.sexy)"
 config.font = wezterm.font("JetBrains Mono")
 config.font_size = 14
 
+local runtime_dir = os.getenv("XDG_RUNTIME_DIR")
+
+if runtime_dir then
+	local pattern = runtime_dir .. "/wayland-*"
+	local socks = wezterm.glob(pattern)
+
+	if #socks > 0 then
+		local sock_path = socks[1]
+		local sock_name = sock_path:match(".*/(.*)$")
+
+		config.environment = {
+			WAYLAND_DISPLAY = sock_name,
+		}
+	end
+end
+
 return config
