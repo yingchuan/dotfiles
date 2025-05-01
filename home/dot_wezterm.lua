e6d019e306b99e8b095ab871cab038214dfd70e0
Richard Chen <richard.chen@ui.com>
2025-05-01 08:22:43 +0800

    fix wezterm always env WAYLAND_DISPLAY==1

--- home/dot_wezterm.lua
+++ home/dot_wezterm.lua
@@ -5,19 +5,23 @@ config.color_scheme = "Belge (terminal.sexy)"
 config.font = wezterm.font("JetBrains Mono")
 config.font_size = 14
 
+config.keys = {
+	{ key = "L", mods = "CTRL", action = wezterm.action.ShowDebugOverlay },
+}
+
 local runtime_dir = os.getenv("XDG_RUNTIME_DIR")
-
 if runtime_dir then
-	local pattern = runtime_dir .. "/wayland-*"
-	local socks = wezterm.glob(pattern)
+	local socks = wezterm.glob(runtime_dir .. "/wayland-[0-9]*")
+	for _, sock_path in ipairs(socks) do
+		if not sock_path:match("%.lock$") then
+			local sock_name = sock_path:match(".*/(.*)$")
+			wezterm.log_info("Detected Wayland socket: " .. sock_name)
 
-	if #socks > 0 then
-		local sock_path = socks[1]
-		local sock_name = sock_path:match(".*/(.*)$")
-
-		config.environment = {
-			WAYLAND_DISPLAY = sock_name,
-		}
+			config.set_environment_variables = {
+				WAYLAND_DISPLAY = sock_name,
+			}
+			break
+		end
 	end
 end
 
