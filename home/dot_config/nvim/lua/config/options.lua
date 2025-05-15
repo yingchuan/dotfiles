511aa06474ac4f24ed2a7aff279f1155a7d57097
Richard Chen <yingchuan.chen.2007@gmail.com>
2025-05-15 23:51:51 +0800

    apt install wezterm and nvim clipboard always osc52

--- home/dot_config/nvim/lua/config/options.lua
+++ home/dot_config/nvim/lua/config/options.lua
@@ -2,7 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.lazyvim_picker = "fzf"
-vim.opt.clipboard = "unnamedplus"
+vim.g.clipboard = "osc52"
 -- LSP Server to use for Python.
 -- Set to "basedpyright" to use basedpyright instead of pyright.
 vim.g.lazyvim_python_lsp = "ruff"
