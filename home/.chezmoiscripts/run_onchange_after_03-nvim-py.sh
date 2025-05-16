3c8bad7a16e10dd955492e5c65710f78e94d86a6
Richard Chen <yingchuan.chen.2007@gmail.com>
2025-05-17 01:42:24 +0800

    refactor chezmoiscripts - bugfix

--- home/.chezmoiscripts/run_onchange_after_03-nvim-py.sh
+++ home/.chezmoiscripts/run_onchange_after_03-nvim-py.sh
@@ -15,7 +15,7 @@ load_brew_env() {
 }
 load_brew_env
 
-# --- 1. Ensure pyenv installed ---
+# --- 1) Ensure pyenv installed ---
 if ! command -v pyenv >/dev/null 2>&1; then
   echo "[x] Installing pyenv..."
   brew install pyenv
@@ -24,7 +24,7 @@ else
   echo "[✓] pyenv already installed."
 fi
 
-# --- 2. Ensure pyenv-virtualenv plugin ---
+# --- 2) Ensure pyenv-virtualenv plugin ---
 if [[ ! -d "$PYENV_ROOT/plugins/pyenv-virtualenv" ]]; then
   echo "[x] Installing pyenv-virtualenv..."
   git clone https://github.com/pyenv/pyenv-virtualenv.git "$PYENV_ROOT/plugins/pyenv-virtualenv"
@@ -33,16 +33,17 @@ else
   echo "[✓] pyenv-virtualenv already present."
 fi
 
-# --- Initialize pyenv and virtualenv ---
+# --- Initialize pyenv environment ---
 export PYENV_ROOT="$PYENV_ROOT"
 export PATH="$PYENV_ROOT/bin:$PATH"
+# Initialize pyenv and virtualenv support
+if command -v pyenv >/dev/null 2>&1; then
+  eval "$(pyenv init --path)"
+  eval "$(pyenv init -)"
+  eval "$(pyenv virtualenv-init -)"
+fi
 
-# shellcheck disable=SC2016
-eval "\$(pyenv init -)"
-# shellcheck disable=SC2016
-eval "\$(pyenv virtualenv-init -)"
-
-# --- 3. Install Python versions ---
+# --- 3) Install Python versions ---
 for version in "${PYTHON_VERSIONS[@]}"; do
   if ! pyenv versions --bare | grep -qx "$version"; then
     echo "[x] Installing Python $version..."
@@ -55,7 +56,7 @@ for version in "${PYTHON_VERSIONS[@]}"; do
   fi
 done
 
-# --- 4. Create virtualenv ---
+# --- 4) Create or link virtualenv ---
 if ! pyenv virtualenvs --bare | grep -qx "$VENV_NAME"; then
   echo "[x] Creating virtualenv $VENV_NAME..."
   pyenv virtualenv "${PYTHON_VERSIONS[0]}" "$VENV_NAME"
@@ -63,10 +64,10 @@ if ! pyenv virtualenvs --bare | grep -qx "$VENV_NAME"; then
   ln -sf "$PYENV_ROOT/versions/$VENV_NAME" "$VENV_PATH"
   echo "[+] Virtualenv $VENV_NAME created at $VENV_PATH."
 else
-  echo "[✓] Virtualenv $VENV_NAME already exists."
+  echo "[✓] Virtualenv $VENV_NAME already exists at $VENV_PATH."
 fi
 
-# --- 5. Install Neovim Python package ---
+# --- 5) Install Neovim Python package ---
 echo "[x] Installing Neovim Python package..."
 "$VENV_PATH/bin/pip" install --upgrade pip
 "$VENV_PATH/bin/pip" install "$NEOVIM_PKG"
