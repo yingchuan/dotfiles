05106307233abdee8dabd581cdb0ead8c9395e47
Richard Chen <yingchuan.chen.2007@gmail.com>
2025-05-17 10:37:55 +0800

    refactor chezmoiscripts - bugfix

--- home/dot_config/nvim/lua/plugins/treesitter.lua
+++ home/dot_config/nvim/lua/plugins/treesitter.lua
@@ -3,7 +3,24 @@ return {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
       if type(opts.ensure_installed) == "table" then
-        vim.list_extend(opts.ensure_installed, { "gotmpl" })
+        vim.list_extend(opts.ensure_installed, {
+          "gotmpl",
+          "go",
+          "c",
+          "zig",
+          "lua",
+          "vim",
+          "cpp",
+          "python",
+          "devicetree",
+          "bitbake",
+          "awk",
+          "jq",
+          "llvm",
+          "tmux",
+          "latex",
+          "typst",
+        })
       end
     end,
   },
